caa152245c2b7e6eaa95de0815987b2352e9e3da
Michael W Powell <mwpowellhtx@gmail.com>
2016-05-12 18:17:17 -0400

    added from json parse method to string prototype

--- src/ExtensionsJS.String/Scripts/ExtensionsJS/str.js
+++ src/ExtensionsJS.String/Scripts/ExtensionsJS/str.js
@@ -1,6 +1,13 @@
-// ReSharper disable once NativeTypePrototypeExtending
+/* ReSharper disable once NativeTypePrototypeExtending */
 if (String.prototype.contains === undefined) {
     String.prototype.contains = function (s) {
         return this.indexOf(s) >= 0;
+    }
+}
+
+/* ReSharper disable once NativeTypePrototypeExtending */
+if (String.prototype.fromJSON === undefined) {
+    String.prototype.fromJSON = function() {
+        return JSON.parse(this);
     }
 }
